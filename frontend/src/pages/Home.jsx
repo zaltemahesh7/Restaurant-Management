cfd229adfcbf2d4893dd18bb270026fd8018caf6
Mahesh Zalte <maheshzalte2000@gmail.com>
2024-04-03 22:42:06 +0530

    Formated code base

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import Navbar from './Navbar'
+import Animation from '../components/Animation'
+import Registration from './Registration'
+
+function Home() {
+  return (
+    <>
+      {/* <Navbar /> */}
+      {/* <Animation /> */}
+      <Registration />
+    </>
+  )
+}
+
+export default Home
