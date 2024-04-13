28c914e5a704956ae7edf637e95f1aed3cbbc1ba
Mahesh Zalte <maheshzalte2000@gmail.com>
2024-04-13 23:40:18 +0530

    UI

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -79,7 +79,7 @@ function Navbar() {
 
         <div className='md:hidden bg-transparent'>
           <button onClick={toggleNavbar}>
-            <svg className="w-6 h-6 bg-transparent" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <svg className="w-6 h-6 bg-transparent mt-7" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
             </svg>
           </button>
--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -1,129 +1,72 @@
-import { json } from 'express';
 import React, { useState } from 'react'
 
 function Registration() {
-    const [user, setUser] = useState({
-        username: '',
-        phone: '',
-        email: '',
-        password: ''
-    })
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [phone, setPhone] = useState('');
+    const [error, setError] = useState('');
 
-    function handleChange(e) {
-        let name = e.target.name;
-        let value = e.target.value;
-        // console.log(name, value);
-        setUser({
-            ...user,
-            [name]: value
-        })
-    }
 
-    const getData = (e) => {
-        e.preventDefault()
-        console.log('Success...', user);
-
-        // Make fields empty
-        setUser({ username: '', phone: '', email: '', password: '' })
-    }
-
-    function handleSubmit(e) {
+    const handleSubmit = async (e) => {
         e.preventDefault();
+
         try {
-            const responce = fetch('http://localhost:3000/register', {
-                method: "POST",
+            const response = await fetch('http://localhost:3000/register', {
+                method: 'POST',
                 headers: {
-                    "Content-Type": "application/json"
+                    'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(user)
-            })
+                body: JSON.stringify({ username, phone, email, password }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Invalid username or password');
+            }
+
+            const data = await response.json();
+            console.log('Login successful:', data);
+            alert('Login success');
+            navigate('/')
+            // Here you can handle successful login, e.g., redirect to dashboard
 
         } catch (error) {
-            console.log('Error: ', error);
+            console.error('Registration error:', error);
+            setError('Invalid username or password');
         }
-    }
+    };
+
 
     return (
-        <>
-            <section className='mt-9 bg-transparent' >
-                <main className='bg-transparent'>
-                    <div className="section-registration bg-transparent">
-                        <div className="container w-full text-center m-auto grid items-center bg-transparent" >
-                            <div className="registration-image bg-transparent">
-                                {/* <img src="/images/img_rectangle150.png" width='500' height='700' alt="Registration Image" /> */}
-                            </div>
-                            <div className="registration-form bg-transparent">
-                                <h1 className='text-6xl my-9 bg-transparent'>Registration</h1>
-                                <br />
+        <div className='flex flex-col w-full gap-8 justify-center items-center '>
+            <div >
+                <h1 className='text-4xl'>Registration Page</h1>
+            </div>
+            <div className='w-[80%]'>
+                <hr className='border-gray-600' />
+            </div>
+            <div className='flex flex-col w-full'>
+                <form method='POST' onSubmit={handleSubmit} className='flex flex-col gap-4 w-full items-center justify-center'>
+                    <label htmlFor="username">UserName</label>
+                    <input className='border' value={username}
+                        onChange={(e) => setUsername(e.target.value)} placeholder='Username' type="text" id='username' name='username' />
 
-                                {/* Registration form */}
+                    <label htmlFor="email">Email</label>
+                    <input className='border' placeholder='Email' value={email}
+                        onChange={(e) => setEmail(e.target.value)} type="email" id='email' name='email' />
 
-                                <form action="" method='post' onSubmit={getData} className='grid gap-3 md:gap-9 sm:gap-4 bg-transparent'>
-                                    <div className='flex flex-col text-left text-xl bg-transparent'>
-                                        {/* <label htmlFor="username">UserName</label> */}
-                                        <input
-                                            type="text"
-                                            placeholder='username'
-                                            className='border p-4 rounded bg-transparent'
-                                            name='username'
-                                            id='username'
-                                            required
-                                            autoComplete='off'
-                                            value={user.username}
-                                            onChange={handleChange}
-                                        />
-                                    </div>
-                                    <div className='flex flex-col text-left text-xl bg-transparent'>
-                                        {/* <label htmlFor="phone">phone</label> */}
-                                        <input
-                                            type="number"
-                                            className='border p-4 rounded bg-transparent'
-                                            placeholder='Phone'
-                                            name='phone'
-                                            id='phone'
-                                            required
-                                            autoComplete='off'
-                                            value={user.phone}
-                                            onChange={handleChange}
-                                        />
-                                    </div>
-                                    <div className='flex flex-col text-left text-xl bg-transparent'>
-                                        {/* <label htmlFor="email">email</label> */}
-                                        <input
-                                            type="email"
-                                            className='border p-4 rounded bg-transparent'
-                                            placeholder='email'
-                                            name='email'
-                                            id='email'
-                                            required
-                                            autoComplete='off'
-                                            value={user.email}
-                                            onChange={handleChange}
-                                        />
-                                    </div>
-                                    <div className='flex flex-col text-left text-xl bg-transparent'>
-                                        {/* <label htmlFor="password">password</label> */}
-                                        <input
-                                            type="password"
-                                            className='border p-4 rounded bg-transparent'
-                                            placeholder='password'
-                                            name='password'
-                                            id='password'
-                                            required a
-                                            utoComplete='off'
-                                            value={user.password}
-                                            onChange={handleChange}
-                                        />
-                                    </div>
-                                    <br />
-                                    <button type='submit' className=' bg-pink-500 py-3 px-5 rounded m-auto' onClick={handleSubmit} > Submit</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </main>
-            </section>
-        </>
+                    <label htmlFor="phone">Phone</label>
+                    <input className='border' placeholder='Phone' value={phone}
+                        onChange={(e) => setPhone(e.target.value)} type="Number" name='phone' id='phone' />
+
+                    <label htmlFor="password">Password</label>
+                    <input className='border' value={password}
+                        onChange={(e) => setPassword(e.target.value)} placeholder='Password' type="password" name='password' id='password' />
+
+                    <button type="submit" className='box-shadow px-4 py-2 bg-red-200 rounded'>Submit</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
